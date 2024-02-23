526993041781e99f5fa089c825412e374da0688e
DragN0007 <cooper.kylie@gmail.com>
2024-02-23 13:31:56 -0600

    -Add Rebuilt Alberto (REBUILT-0.5)

--- src/main/java/com/dragn0007/deadlydinos/entity/carni/Alberto.java
+++ src/main/java/com/dragn0007/deadlydinos/entity/carni/Alberto.java
@@ -34,6 +34,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -119,25 +120,35 @@ public class Alberto extends Animal implements IAnimatable {
     }
 
 
+    //Animation
     private <E extends IAnimatable>PlayState predicate(AnimationEvent<E> event) {
 
         if (event.isMoving()) {
-            if (isAggressive()) {
-                event.getController().setAnimation(new AnimationBuilder().addAnimation("albertosprint", ILoopType.EDefaultLoopTypes.LOOP));
+            if (isAggressive() || isSprinting()) {
+                event.getController().setAnimation(new AnimationBuilder().addAnimation("run", ILoopType.EDefaultLoopTypes.LOOP));
 
             } else
-                event.getController().setAnimation(new AnimationBuilder().addAnimation("albertowalk", ILoopType.EDefaultLoopTypes.LOOP));
-
+                event.getController().setAnimation(new AnimationBuilder().addAnimation("walk", ILoopType.EDefaultLoopTypes.LOOP));
         } else
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("albertoidle", ILoopType.EDefaultLoopTypes.LOOP));
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", ILoopType.EDefaultLoopTypes.LOOP));
 
         return PlayState.CONTINUE;
     }
 
+    private PlayState attackPredicate(AnimationEvent event) {
+        if (this.swinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
+            event.getController().markNeedsReload();
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("attack", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            this.swinging = false;
+        }
+
+        return PlayState.CONTINUE;
+    }
 
     @Override
-    public void registerControllers(AnimationData data) {
-        data.addAnimationController(new AnimationController(this,"controller",5,this::predicate));
+    public void registerControllers (AnimationData data){
+        data.addAnimationController(new AnimationController(this, "controller", 3, this::predicate));
+        data.addAnimationController(new AnimationController(this, "attackController", 1, this::attackPredicate));
     }
 
     @Override
