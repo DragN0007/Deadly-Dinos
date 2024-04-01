eeda69a3fc848babbc5e281c7ba6defe489cd0f0
DragN0007 <cooper.kylie@gmail.com>
2024-04-01 02:20:03 -0500

    -Add Rebuilt Majunga (REBUILT-0.6)

--- src/main/java/com/dragn0007/deadlydinos/entity/carni/Majunga.java
+++ src/main/java/com/dragn0007/deadlydinos/entity/carni/Majunga.java
@@ -34,6 +34,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
+import software.bernie.geckolib3.core.AnimationState;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -61,7 +62,7 @@ public class Majunga extends Animal implements IAnimatable {
         return Mob.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 60)
                 .add(Attributes.ATTACK_DAMAGE, 6)
-                .add(Attributes.MOVEMENT_SPEED, 0.2)
+                .add(Attributes.MOVEMENT_SPEED, 0.23)
                 .add(Attributes.KNOCKBACK_RESISTANCE, 0.5)
                 ;
     }
@@ -85,7 +86,7 @@ public class Majunga extends Animal implements IAnimatable {
         this.goalSelector.addGoal(0, new HurtByTargetGoal(this));
         this.goalSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, Player.class, 40, true, true, LivingEntity::attackable));
         this.goalSelector.addGoal(2, new BreakDoorGoal(this, (x) -> x == Difficulty.EASY || x == Difficulty.NORMAL || x == Difficulty.HARD));
-        this.goalSelector.addGoal(0, new DinoWeakMeleeGoal(this, 1.8, true));
+        this.goalSelector.addGoal(0, new DinoWeakMeleeGoal(this, 1.6, true));
         this.goalSelector.addGoal(4, new FloatGoal(this));
         this.goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 1));
         this.targetSelector.addGoal(0, new NearestAttackableTargetGoal<>(this, LivingEntity.class, 10, true, true, new Predicate<LivingEntity>() {
@@ -115,25 +116,35 @@ public class Majunga extends Animal implements IAnimatable {
     }
 
 
+    //Animation
     private <E extends IAnimatable>PlayState predicate(AnimationEvent<E> event) {
 
         if (event.isMoving()) {
-            if (isAggressive()) {
-                event.getController().setAnimation(new AnimationBuilder().addAnimation("majungasprint", ILoopType.EDefaultLoopTypes.LOOP));
+            if (isAggressive() || isSprinting()) {
+                event.getController().setAnimation(new AnimationBuilder().addAnimation("run", ILoopType.EDefaultLoopTypes.LOOP));
 
             } else
-                event.getController().setAnimation(new AnimationBuilder().addAnimation("majungawalk", ILoopType.EDefaultLoopTypes.LOOP));
-
+                event.getController().setAnimation(new AnimationBuilder().addAnimation("walk", ILoopType.EDefaultLoopTypes.LOOP));
         } else
-            event.getController().setAnimation(new AnimationBuilder().addAnimation("majungaidle", ILoopType.EDefaultLoopTypes.LOOP));
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", ILoopType.EDefaultLoopTypes.LOOP));
 
         return PlayState.CONTINUE;
     }
 
+    private PlayState attackPredicate(AnimationEvent event) {
+        if (this.swinging && event.getController().getAnimationState().equals(AnimationState.Stopped)) {
+            event.getController().markNeedsReload();
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("attack", ILoopType.EDefaultLoopTypes.PLAY_ONCE));
+            this.swinging = false;
+        }
+
+        return PlayState.CONTINUE;
+    }
 
     @Override
-    public void registerControllers(AnimationData data) {
-        data.addAnimationController(new AnimationController(this, "controller", 5, this::predicate));
+    public void registerControllers (AnimationData data){
+        data.addAnimationController(new AnimationController(this, "controller", 3, this::predicate));
+        data.addAnimationController(new AnimationController(this, "attackController", 1, this::attackPredicate));
     }
 
     @Override
