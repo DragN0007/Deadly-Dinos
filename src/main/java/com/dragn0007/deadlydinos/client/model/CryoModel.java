3649f213a86e67199df688d823e15ee3df8705e5
DragN0007 <cooper.kylie@gmail.com>
2024-05-16 16:28:26 -0500

    -Add Cryolophosaurus (1.8)

---
+++ src/main/java/com/dragn0007/deadlydinos/client/model/CryoModel.java
@@ -0,0 +1,43 @@
+package com.dragn0007.deadlydinos.client.model;
+
+import com.dragn0007.deadlydinos.DeadlyDinos;
+import com.dragn0007.deadlydinos.entity.carni.Cryo;
+import com.dragn0007.deadlydinos.entity.carni.Yuty;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class CryoModel extends AnimatedGeoModel<Cryo> {
+
+    public enum Variant {
+        MALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/cryomale.png")),
+        FEMALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/cryofemale.png")),
+        ICY(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/cryoicy.png"));
+
+        public final ResourceLocation resourceLocation;
+        Variant(ResourceLocation resourceLocation) {
+            this.resourceLocation = resourceLocation;
+        }
+
+        public static Variant variantFromOrdinal(int variant) { return Variant.values()[variant % Variant.values().length];
+        }
+    }
+
+    public static final ResourceLocation model = new ResourceLocation(DeadlyDinos.MODID, "geo/cryo.geo.json");
+    public static final ResourceLocation animation = new ResourceLocation(DeadlyDinos.MODID, "animations/cryo.animation.json");
+
+
+    @Override
+    public ResourceLocation getModelLocation(Cryo object) {
+        return model;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(Cryo object) {
+        return object.getTextureLocation();
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(Cryo animatable) {
+        return animation;
+    }
+}
---
+++ src/main/java/com/dragn0007/deadlydinos/client/render/CryoRender.java
@@ -0,0 +1,14 @@
+package com.dragn0007.deadlydinos.client.render;
+
+import com.dragn0007.deadlydinos.client.model.CryoModel;
+import com.dragn0007.deadlydinos.client.model.RexModel;
+import com.dragn0007.deadlydinos.entity.carni.Cryo;
+import com.dragn0007.deadlydinos.entity.carni.Rex;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class CryoRender extends GeoEntityRenderer<Cryo> {
+    public CryoRender(EntityRendererProvider.Context renderManager) {
+        super(renderManager, new CryoModel());
+    }
+}
