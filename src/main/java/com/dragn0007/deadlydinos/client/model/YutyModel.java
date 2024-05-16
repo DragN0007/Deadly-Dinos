79f6a967460252d7ac11dc8d7c5b5ca93c0bfa5d
DragN0007 <cooper.kylie@gmail.com>
2024-05-16 16:28:48 -0500

    -Add Yutyrannus (1.8)

---
+++ src/main/java/com/dragn0007/deadlydinos/client/model/YutyModel.java
@@ -0,0 +1,44 @@
+package com.dragn0007.deadlydinos.client.model;
+
+import com.dragn0007.deadlydinos.DeadlyDinos;
+import com.dragn0007.deadlydinos.entity.carni.Allo;
+import com.dragn0007.deadlydinos.entity.carni.Yuty;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class YutyModel extends AnimatedGeoModel<Yuty> {
+
+    public enum Variant {
+        MALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/yutymale.png")),
+        FEMALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/yutyfemale.png")),
+        ICY(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/yutyicy.png"));
+
+        public final ResourceLocation resourceLocation;
+        Variant(ResourceLocation resourceLocation) {
+            this.resourceLocation = resourceLocation;
+        }
+
+        public static Variant variantFromOrdinal(int variant) { return Variant.values()[variant % Variant.values().length];
+        }
+    }
+
+    public static final ResourceLocation model = new ResourceLocation(DeadlyDinos.MODID, "geo/yuty.geo.json");
+    public static final ResourceLocation animation = new ResourceLocation(DeadlyDinos.MODID, "animations/yuty.animation.json");
+
+
+
+    @Override
+    public ResourceLocation getModelLocation(Yuty object) {
+        return model;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(Yuty object) {
+        return object.getTextureLocation();
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(Yuty animatable) {
+        return animation;
+    }
+}
---
+++ src/main/java/com/dragn0007/deadlydinos/client/render/YutyRender.java
@@ -0,0 +1,101 @@
+package com.dragn0007.deadlydinos.client.render;
+
+import com.dragn0007.deadlydinos.client.model.YutyModel;
+import com.dragn0007.deadlydinos.entity.carni.Yuty;
+import com.dragn0007.deadlydinos.entity.carni.Yuty;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib3.core.processor.IBone;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
+import software.bernie.geckolib3.geo.render.built.GeoModel;
+import software.bernie.geckolib3.renderers.geo.ExtendedGeoEntityRenderer;
+
+public class YutyRender extends ExtendedGeoEntityRenderer<Yuty> {
+    public YutyRender(EntityRendererProvider.Context renderManager) {
+        super(renderManager, new YutyModel());
+    }
+
+    @Override
+    public RenderType getRenderType(Yuty animatable, float partialTicks, PoseStack stack,
+                                    MultiBufferSource renderTypeBuffer, VertexConsumer vertexBuilder, int packedLightIn,
+                                    ResourceLocation textureLocation) {
+        if(animatable.isBaby()) {
+            stack.scale(0.4F, 0.4F, 0.4F);
+        } else {
+            stack.scale(1F, 1F, 1F);
+        }
+
+        return super.getRenderType(animatable, partialTicks, stack, renderTypeBuffer, vertexBuilder, packedLightIn, textureLocation);
+    }
+
+    @Override
+    public void render(GeoModel model, Yuty animatable, float partialTick, RenderType type, PoseStack poseStack, MultiBufferSource bufferSource, VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+
+        if(animatable.isSaddled()) {
+            model.getBone("Saddle").ifPresent(b -> b.setHidden(false));
+            model.getBone("Saddle3").ifPresent(b -> b.setHidden(false));
+        } else {
+            model.getBone("Saddle").ifPresent(b -> b.setHidden(true));
+            model.getBone("Saddle3").ifPresent(b -> b.setHidden(true));
+        }
+
+        super.render(model, animatable, partialTick, type, poseStack, bufferSource, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+    }
+    
+    @Override
+    protected boolean isArmorBone(GeoBone bone) {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    protected ResourceLocation getTextureForBone(String boneName, Yuty animatable) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected ItemStack getHeldItemForBone(String boneName, Yuty animatable) {
+        return null;
+    }
+
+    @Override
+    protected ItemTransforms.TransformType getCameraTransformForItemAtBone(ItemStack stack, String boneName) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected BlockState getHeldBlockForBone(String boneName, Yuty animatable) {
+        return null;
+    }
+
+    @Override
+    protected void preRenderItem(PoseStack poseStack, ItemStack stack, String boneName, Yuty animatable, IBone bone) {
+
+    }
+
+    @Override
+    protected void preRenderBlock(PoseStack poseStack, BlockState state, String boneName, Yuty animatable) {
+
+    }
+
+    @Override
+    protected void postRenderItem(PoseStack poseStack, ItemStack stack, String boneName, Yuty animatable, IBone bone) {
+
+    }
+
+    @Override
+    protected void postRenderBlock(PoseStack poseStack, BlockState state, String boneName, Yuty animatable) {
+
+    }
+}
+
