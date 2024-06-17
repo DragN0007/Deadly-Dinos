0ec007c4274c79f1072b1240c707c05a77362a27
kylie <cooper.kylie1077@gmail.com>
2024-06-16 23:21:20 -0500

    -Add Edmontosaurus (2.0)

---
+++ src/main/java/com/dragn0007/deadlydinos/client/model/EdmontoModel.java
@@ -0,0 +1,44 @@
+package com.dragn0007.deadlydinos.client.model;
+
+import com.dragn0007.deadlydinos.DeadlyDinos;
+import com.dragn0007.deadlydinos.entity.herbi.Edmonto;
+import com.dragn0007.deadlydinos.entity.herbi.Shant;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class EdmontoModel extends AnimatedGeoModel<Edmonto> {
+    
+    public enum Variant {
+        MALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/edmontomale.png")),
+        FEMALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/edmontofemale.png")),
+        PLUM(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/edmontoplum.png"));
+        
+        public final ResourceLocation resourceLocation;
+        Variant(ResourceLocation resourceLocation) {
+            this.resourceLocation = resourceLocation;
+        }
+
+        public static Variant variantFromOrdinal(int variant) { return Variant.values()[variant % Variant.values().length];
+        }
+    }
+
+    public static final ResourceLocation model = new ResourceLocation(DeadlyDinos.MODID, "geo/edmonto.geo.json");
+    public static final ResourceLocation animation = new ResourceLocation(DeadlyDinos.MODID, "animations/edmonto.animation.json");
+
+
+
+    @Override
+    public ResourceLocation getModelLocation(Edmonto object) {
+        return model;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(Edmonto object) {
+        return object.getTextureLocation();
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(Edmonto animatable) {
+        return animation;
+    }
+}
---
+++ src/main/java/com/dragn0007/deadlydinos/client/render/EdmontoRender.java
@@ -0,0 +1,14 @@
+package com.dragn0007.deadlydinos.client.render;
+
+import com.dragn0007.deadlydinos.client.model.EdmontoModel;
+import com.dragn0007.deadlydinos.client.model.ShantModel;
+import com.dragn0007.deadlydinos.entity.herbi.Edmonto;
+import com.dragn0007.deadlydinos.entity.herbi.Shant;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class EdmontoRender extends GeoEntityRenderer<Edmonto> {
+    public EdmontoRender(EntityRendererProvider.Context renderManager) {
+        super(renderManager, new EdmontoModel());
+    }
+}
