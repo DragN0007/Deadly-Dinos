c530dd954f018d893624cd6c1c731018668e1407
kylie <cooper.kylie1077@gmail.com>
2024-06-19 20:22:24 -0500

    -Add Iguanodon (2.0)

---
+++ src/main/java/com/dragn0007/deadlydinos/client/model/IguaModel.java
@@ -0,0 +1,41 @@
+package com.dragn0007.deadlydinos.client.model;
+
+import com.dragn0007.deadlydinos.DeadlyDinos;
+import com.dragn0007.deadlydinos.entity.herbi.Igua;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class IguaModel extends AnimatedGeoModel<Igua> {
+
+    public enum Variant {
+        MALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/iguamale.png")),
+        FEMALE(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/iguafemale.png")),
+        PERIDOT(new ResourceLocation(DeadlyDinos.MODID, "textures/entity/iguagold.png"));
+
+        public final ResourceLocation resourceLocation;
+        Variant(ResourceLocation resourceLocation) {
+            this.resourceLocation = resourceLocation;
+        }
+
+        public static Variant variantFromOrdinal(int variant) { return Variant.values()[variant % Variant.values().length];
+        }
+    }
+
+    public static final ResourceLocation model = new ResourceLocation(DeadlyDinos.MODID, "geo/igua.geo.json");
+    public static final ResourceLocation animation = new ResourceLocation(DeadlyDinos.MODID, "animations/igua.animation.json");
+
+
+
+    @Override
+    public ResourceLocation getModelLocation(Igua object) {
+        return model;
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(Igua object) {return object.getTextureLocation();}
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(Igua animatable) {
+        return animation;
+    }
+}
