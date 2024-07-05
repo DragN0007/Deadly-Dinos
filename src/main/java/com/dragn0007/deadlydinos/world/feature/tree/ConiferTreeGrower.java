6ad51098f74efd7d1d0c36863711bb6ee23b3bdb
kylie <cooper.kylie1077@gmail.com>
2024-07-04 20:05:08 -0500

    -Fix mega tree saplings (2.1)

--- src/main/java/com/dragn0007/deadlydinos/world/feature/tree/ConiferTreeGrower.java
+++ src/main/java/com/dragn0007/deadlydinos/world/feature/tree/ConiferTreeGrower.java
@@ -2,16 +2,23 @@ package com.dragn0007.deadlydinos.world.feature.tree;
 
 import com.dragn0007.deadlydinos.world.feature.DDDConfigFeatures;
 import net.minecraft.core.Holder;
+import net.minecraft.world.level.block.grower.AbstractMegaTreeGrower;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
-public class ConiferTreeGrower extends AbstractTreeGrower {
+public class ConiferTreeGrower extends AbstractMegaTreeGrower {
+    @Nullable
+    @Override
+    protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredMegaFeature(Random p_204306_) {
+        return DDDConfigFeatures.CONIFER;
+    }
+
     @Nullable
     @Override
     protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(Random p_204307_, boolean p_204308_) {
-        return DDDConfigFeatures.CONIFER;
+        return null;
     }
 }
